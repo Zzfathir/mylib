d1a7dcdd74120d9656570de06400bfb418754b32
Zzfathir <ahmadfathir0830@gmail.com>
2023-03-21 15:10:15 +0700

    make book resource

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,7 @@ class BookController extends Controller
     public function index() {
         $books = Book::all();
 
-        return $books;
+        return BookResource::collection($books->loadMissing('pustakawan:id,name'));
     }
 
 
