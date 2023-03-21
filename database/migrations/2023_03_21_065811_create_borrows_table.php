239391f7b3c4afb3560be7353a9a4b6bf41dbd6a
Zzfathir <ahmadfathir0830@gmail.com>
2023-03-21 14:18:27 +0700

    make user, books & borrows table

--- database/migrations/2023_03_21_065811_create_borrows_table.php
+++ database/migrations/2023_03_21_065811_create_borrows_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('borrows', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
-            $table->softDeletes();
-
-            
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('borrows');
     }
 };
